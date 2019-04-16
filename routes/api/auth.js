3bcc169073053a410d7f366d84830d54f26b6171
Thomas Roberts <robertsthomasd@gmail.com>
2019-04-16 09:08:21 -0700

    Create auth for users

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,6 +4,7 @@ const User = require('../../models/User') // User Model
 const bcrypt = require('bcryptjs')
 const config = require('config')
 const jwt = require('jsonwebtoken')
+const auth = require('../../middleware/auth')
 
 // @route POST api/auth
 // @desc Auth User
@@ -11,7 +12,7 @@ const jwt = require('jsonwebtoken')
 router.post('/', (req, res) => {
 	const { email, password } = req.body
 
-	if ( !email || !password) {
+	if (!email || !password) {
 		return res.status(400).json({ msg: 'Please enter all fields.' })
 	}
 
@@ -19,41 +20,39 @@ router.post('/', (req, res) => {
 	User.findOne({ email }).then(user => {
 		if (!user) return res.status(400).json({ msg: 'User does not exist.' })
 
-		const newUser = new User({
-			name,
-			email,
-			password
-		})
+		// Validate password
+		bcrypt.compare(password, user.password).then(isMatch => {
+			if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" })
 
-		// Create salt and hash
-		bcrypt.genSalt(10, (err, salt) => {
-			bcrypt.hash(newUser.password, salt, (err, hash) => {
-				if (err) throw err;
+			jwt.sign(
+				{ id: user.id },
+				config.get("jwtSecret"),
+				{ expiresIn: 3600 },
+				(err, token) => {
+					if (err) throw err;
 
-				newUser.password = hash
-				newUser.save().then(user => {
-
-					jwt.sign(
-						{ id: user.id },
-						config.get("jwtSecret"),
-						{ expiresIn: 3600 },
-						(err, token) => {
-							if (err) throw err;
-
-							res.json({
-								token,
-								user: {
-									id: user.id,
-									name: user.name,
-									email: user.email
-								}
-							})
+					res.json({
+						token,
+						user: {
+							id: user.id,
+							name: user.name,
+							email: user.email
 						}
-					)
-				})
-			})
+					})
+				}
+			)
 		})
 	})
+})
+
+// @route GET api/auth/user
+// @desc Get User data
+// @access Private
+
+router.get('/user', auth, (req, res) => {
+	User.findById(req.user.id)
+		.select("-password")
+		.then(user => res.json(user))
 })
 
 
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(cors())
 
 app.use('/api/items', require('./routes/api/items'))
 app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
 
 
 //  Serve static assets if in production
